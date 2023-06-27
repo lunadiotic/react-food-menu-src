f1274257b98443c398e32a3f88b3e0a24f2ef1ae
aimeliala <idstack@outlook.co.id>
2023-06-27 21:49:26 +0700

    mempercantik component dengan css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -14,17 +16,37 @@ function App() {
 // child component ditulis di bawah parent component
 // Tidak boleh di bawahnya
 function Header() {
-  return <h1>Warteg Mang Udin</h1>;
+  // const style = {
+  //   color: "red",
+  //   fontSize: "48px",
+  //   textTransform: "uppercase",
+  // };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1
+        // style={{
+        //   color: "red",
+        //   fontSize: "32px",
+        //   textTransform: "uppercase",
+        // }}
+        style={style}
+      >
+        Warteg Mang Udin
+      </h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Menu kita</h2>
       <Food></Food>
       <Food />
       <Food />
-    </div>
+    </main>
   );
 }
 
@@ -39,14 +61,18 @@ function Footer() {
     alert("Yes kita buka");
   }
 
-  return <footer>{new Date().toLocaleTimeString()}. Warteg Mang Udin.</footer>;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. Warteg Mang Udin.
+    </footer>
+  );
 }
 
 function Food() {
   return (
     <div>
-      <img src="food/nasi-goreng.jpg" alt="" width={100} height={70} />
-      <h2>Nasi Goreng Mang Udin</h2>
+      <img src="food/nasi-goreng.jpg" alt="" width={200} height={140} />
+      <h3>Nasi Goreng Mang Udin</h3>
       <p>Ini adalah nasi goreng</p>
     </div>
   );
