8ed3199792eb1d83c0545917d1ca0d953ba9b9d4
aimeliala <idstack@outlook.co.id>
2023-06-27 10:57:51 +0700

    cara membuat dan menggunakan component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <Food />
+    </div>
+  );
+}
+
+// child component ditulis di bawah parent component
+// Tidak boleh di bawahnya
+function Food() {
+  return (
+    <div>
+      <img src="food/nasi-goreng.jpg" alt="" />
+      <h2>Nasi Goreng</h2>
+      <p>Nasi Goreng mang udin</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
